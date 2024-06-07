72790aae6b0cb24a3a3ab29f3f2bf1ef53c4fe08
Toleu Bahauddin <baha200477@gmail.com>
2024-06-08 00:24:59 +0500

    check deploy

--- backend/helpers/db.js
+++ backend/helpers/db.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
 
 const mongooseConnection = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mernblog');
+
+    const mongoUrl = process.env.MONGODB_URL;
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
