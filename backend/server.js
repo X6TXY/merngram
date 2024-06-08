6fc8ce09bac598624ae3a7e379be7c45a8b0e226
Toleu Bahauddin <baha200477@gmail.com>
2024-06-08 15:04:53 +0500

    check deploy

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const io = socketIo(server, {
 
 const Message = require('./models/message'); 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
